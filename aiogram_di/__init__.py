6d4428122c2fd9c449ec8307162bf0936f9a375f
Maxim <maximzayats1@gmail.com>
2022-10-21 20:34:30 +0300

    refactor: update internal logic

--- aiogram_di/__init__.py
+++ aiogram_di/__init__.py
@@ -1,26 +1,41 @@
+from __future__ import annotations
+
 from asyncio import iscoroutine
 from inspect import FullArgSpec
-from typing import Any, Awaitable, Callable, Dict, Union
+from typing import Any, Awaitable, Callable, Union
 
 from aiogram import BaseMiddleware
 from aiogram.dispatcher.event.handler import HandlerObject
 from aiogram.types import TelegramObject
 
-__all__ = ["DIMiddleware"]
+__all__ = [
+    "DIMiddleware",
+    "SyncDependencyResolverType",
+    "AsyncDependencyResolverType",
+    "DependencyResolverType",
+]
+
+SyncDependencyResolverType = Callable[[], Any]
+AsyncDependencyResolverType = Callable[[], Awaitable[Any]]
+
+DependencyResolverType = Union[SyncDependencyResolverType, AsyncDependencyResolverType]
 
 
 class DIMiddleware(BaseMiddleware):
     def __init__(
         self,
-        dependencies: Dict[type, Union[Callable[[], Any], Callable[[], Awaitable[Any]]]],
+        dependencies: dict[type, DependencyResolverType],
     ) -> None:
         self._dependencies = dependencies
+        self._handlers_dependencies_resolvers: dict[
+            int, dict[str, DependencyResolverType]
+        ] = {}
 
     def _get_dependencies_resolvers(
         self,
         func_spec: FullArgSpec,
-    ) -> Dict[str, Callable[[], Any]]:
-        dependencies_resolvers = {}
+    ) -> dict[str, DependencyResolverType]:
+        dependencies_resolvers: dict[str, DependencyResolverType] = {}
 
         for arg_name, arg_type in func_spec.annotations.items():
             if arg_name == "return":
@@ -33,31 +48,26 @@ class DIMiddleware(BaseMiddleware):
 
     async def __call__(
         self,
-        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
         event: TelegramObject,
-        data: Dict[str, Any],
+        data: dict[str, Any],
     ) -> Any:
         handler_obj: HandlerObject = handler.__wrapped__.__self__  # type: ignore
+        handler_id = id(handler_obj)
 
-        if not hasattr(handler_obj, "dependencies_resolvers"):
-            setattr(
-                handler_obj,
-                "dependencies_resolvers",
-                self._get_dependencies_resolvers(
-                    handler_obj.spec,
-                ),
-            )
+        if handler_id not in self._handlers_dependencies_resolvers:
+            self._handlers_dependencies_resolvers[
+                handler_id
+            ] = self._get_dependencies_resolvers(handler_obj.spec)
 
-        dependencies_resolvers: Dict[
-            str, Union[Callable[[], Any], Callable[[], Awaitable[Any]]]
-        ] = getattr(handler_obj, "dependencies_resolvers")
+        for arg_name, resolver in self._handlers_dependencies_resolvers[
+            handler_id
+        ].items():
+            value = resolver()
 
-        for arg_name, resolver_function in dependencies_resolvers.items():
-            dependant = resolver_function()
-
-            if iscoroutine(dependant):
-                data[arg_name] = await dependant
+            if iscoroutine(value):
+                data[arg_name] = await value
             else:
-                data[arg_name] = dependant
+                data[arg_name] = value
 
         return await handler(event, data)
